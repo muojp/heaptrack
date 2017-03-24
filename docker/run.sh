22dcf38bdbc5aaf716242bf2180c1fffb86ddfeb
Kei Nakazawa <muo@muo.jp>
2017-03-24 19:20:46 +0900

    Remove SSH from prerequisites

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 service ssh start > /dev/null 2>&1
-/noVNC/utils/launch.sh --vnc localhost:5900 > /dev/null 2>&1 &
+/opt/noVNC/utils/launch.sh --vnc localhost:5900 > /dev/null 2>&1 &
 /usr/bin/Xvfb :0 -screen 0 1332x896x24 -cc 4 -nolisten tcp > /dev/null 2>&1 &
 x11vnc -display :0 -noipv6 -forever -geometry 1070x720 > /dev/null 2>&1 &
 echo "===>  http://localhost:6080/"
